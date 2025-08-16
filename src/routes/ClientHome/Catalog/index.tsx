5f2ab42c63f710b49f95b708862597c7718c6130
Hyde-R <hydewinter04@gmail.com>
2025-08-16 20:32:54 -0300

    Adding React Router DOM

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -1,13 +1,10 @@
 import "./styles.css";
-import HeaderClient from "../../components/HeaderClient";
-import SearchBar from "../../components/SearchBar";
-import CatalogCard from "../../components/CatalogCard";
-import ButtonNextPage from "../../components/ButtonNextPage";
+import SearchBar from "../../../components/SearchBar";
+import CatalogCard from "../../../components/CatalogCard";
+import ButtonNextPage from "../../../components/ButtonNextPage";
 
 export default function Catalog() {
   return (
-    <>
-      <HeaderClient />
       <main>
         <section id="catalog-section" className="dsc-container">
           <SearchBar />
@@ -28,7 +25,5 @@ export default function Catalog() {
           <ButtonNextPage/>
         </section>
       </main>
-      ;
-    </>
   );
 }
--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -1,15 +1,12 @@
 import './styles.css';
 
-import ButtonInverse from "../../components/ButtonInverse";
-import ButtonPrimary from "../../components/ButtonPrimary";
-import HeaderClient from "../../components/HeaderClient";
-import ProductDetailsCard from "../../components/ProductDetailsCard";
+import ButtonInverse from "../../../components/ButtonInverse";
+import ButtonPrimary from "../../../components/ButtonPrimary";
+import ProductDetailsCard from "../../../components/ProductDetailsCard";
 
 export default function ProductDetails() {
 
   return (
-    <>
-      <HeaderClient/>
       <main>
         <section id="product-details-section" className="dsc-container">
           <ProductDetailsCard />
@@ -19,6 +16,5 @@ export default function ProductDetails() {
           </div>
         </section>
       </main>
-    </>
   );
 }
