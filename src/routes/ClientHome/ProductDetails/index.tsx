82fb22849a109459152419aeea6e10d127ea6834
Hyde-R <hydewinter04@gmail.com>
2025-09-18 17:53:08 -0300

    Login form, login request

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import "./styles.css";
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
--- src/services/product-service.ts
+++ src/services/product-service.ts
@@ -1,22 +1,26 @@
-import axios, { type AxiosRequestConfig } from "axios";
-import { BASE_URL } from "../utils/system";
+import { type AxiosRequestConfig } from "axios";
+import { requestBackend } from "../utils/requests";
 
-export function findPageRequest(page: number, name: string, size = 12, sort = "name") {
+export function findPageRequest(
+  page: number,
+  name: string,
+  size = 12,
+  sort = "name"
+) {
   const config: AxiosRequestConfig = {
     method: "GET",
-    baseURL: BASE_URL,
     url: "/products",
     params: {
-        page,
-        name,
-        size,
-        sort
+      page,
+      name,
+      size,
+      sort,
     },
   };
 
-  return axios(config);
+  return requestBackend(config);
 }
 
 export function findById(id: number) {
-  return axios.get(`${BASE_URL}/products/${id}`);
+  return requestBackend({ url: `/products/${id}` });
 }
