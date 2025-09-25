f003d35cdae8cf554d451175ba342a9a8716cab1
Hyde-R <hydewinter04@gmail.com>
2025-09-24 22:07:33 -0300

    Redirecting outside React component

--- src/routes/Admin/AdminHome/index.tsx
+++ src/routes/Admin/AdminHome/index.tsx
@@ -5,16 +5,15 @@ import type { UserDTO } from "../../../models/user";
 import * as userService from "../../../services/user-service";
 
 export default function AdminHome() {
+
   const [user, setUser] = useState<UserDTO>();
 
   useEffect(() => {
     userService.findMe().then((response) => {
       setUser(response.data);
       console.log(response.data);
-    }).catch(error => {
-      console.log("Error ", error)
     })
-  }, [user]);
+  }, []);
 
   return (
     <main>
