c5734b824cef0cf3e83fc1c4b0acd8f8705f5858
Hyde-R <hydewinter04@gmail.com>
2025-09-29 22:04:07 -0300

    PrivateRoute

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -5,7 +5,6 @@ import ButtonNextPage from "../../../components/ButtonNextPage";
 import { useEffect, useState } from "react";
 import type { ProductDTO } from "../../../models/product";
 import * as productService from "../../../services/product-service";
-import { isAuthenticated } from "../../../services/auth-service";
 
 type QueryParams = {
   page: number;
@@ -21,7 +20,6 @@ export default function Catalog() {
     name: "",
   });
   useEffect(() => {
-    console.log("AUTENTICADO" , isAuthenticated())
     productService
       .findPageRequest(queryParams.page, queryParams.name)
       .then((response) => {
