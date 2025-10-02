7dc09b2ea30690136e14dff6d656aec5a7b7a6bf
Hyde-R <hydewinter04@gmail.com>
2025-10-02 17:59:34 -0300

    Confirmation

---
+++ src/services/order-service.ts
@@ -0,0 +1,15 @@
+import type { AxiosRequestConfig } from "axios";
+import { requestBackend } from "../utils/requests";
+
+
+export function findByIdRequest(id: number){
+
+    const config : AxiosRequestConfig = {
+        method: 'GET',
+        url: `/orders/${id}`,
+        withCredentials: true,
+    }
+
+    return requestBackend(config);
+
+}
