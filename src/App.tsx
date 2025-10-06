89b5618bb816d3956a20f13eee8037d6f7553bb3
Hyde-R <hydewinter04@gmail.com>
2025-10-06 12:57:19 -0300

    Adding productListing and update product routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { ContextCartCount } from "./utils/context-cart";
+import { unstable_HistoryRouter as HistoryRouter } from "react-router-dom";
+import { history } from "./utils/history";
+import { PrivateRoute } from "./components/PrivateRoute";
+import { ContextToken } from "./utils/context-token";
+
+import type { AccessTokenPayloadDTO } from "./models/auth";
+
+import Confirmation from "./routes/ClientHome/Confirmation";
+import ProductListing from "./routes/Admin/ProductListing";
 import ProductDetails from "./routes/ClientHome/ProductDetails";
 import ClientHome from "./routes/ClientHome";
 import Catalog from "./routes/ClientHome/Catalog";
 import Cart from "./routes/ClientHome/Cart";
-import { useEffect, useState } from "react";
-import { ContextCartCount } from "./utils/context-cart";
 import Login from "./routes/ClientHome/Login";
 import Admin from "./routes/Admin";
 import AdminHome from "./routes/Admin/AdminHome";
-import { unstable_HistoryRouter as HistoryRouter } from "react-router-dom";
-import { history } from "./utils/history";
-import { PrivateRoute } from "./components/PrivateRoute";
-import type { AccessTokenPayloadDTO } from "./models/auth";
-import { ContextToken } from "./utils/context-token";
+
 import * as authService from "./services/auth-service";
 import * as cartService from "./services/cart-service";
-import Confirmation from "./routes/ClientHome/Confirmation";
+import ProductForm from "./routes/Admin/ProductForm";
 
 export default function App() {
   const [contextCartCount, setContextCartCount] = useState<number>(0);
@@ -43,30 +48,24 @@ export default function App() {
             <Route path="/" element={<ClientHome />}>
               <Route index element={<Catalog />} />
               <Route path="catalog" element={<Catalog />} />
-              <Route
-                path="product-details/:productId"
-                element={<ProductDetails />}
-              />
+              <Route path="product-details/:productId" element={<ProductDetails />}/>
               <Route path="cart" element={<Cart />} />
               <Route path="login" element={<Login />} />
-              <Route
-                path="confirmation/:orderId"
-                element={
+              <Route path="confirmation/:orderId" element={
                   <PrivateRoute>
                     <Confirmation />
                   </PrivateRoute>
-                }
-              />
+                }/>
             </Route>
-            <Route
-              path="/admin"
-              element={
+            <Route path="/admin/" element={
                 <PrivateRoute roles={["ROLE_ADMIN"]}>
                   <Admin />
                 </PrivateRoute>
-              }
-            >
-              <Route index element={<AdminHome />}></Route>
+              }>
+              <Route index element={<Navigate to="/admin/home"/>}/>
+              <Route path="home" element={<AdminHome />}/>
+              <Route path="products" element={<ProductListing />} />
+              <Route path="products/:productId" element={<ProductForm />} />
             </Route>
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
