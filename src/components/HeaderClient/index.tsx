fe2e58de5213e3da5149d5794a7a16f4e6c6215c
Maxwell-Luan <hydewinter04@gmail.com>
2026-01-14 21:42:09 -0300

    changing title to default

--- src/components/HeaderClient/index.tsx
+++ src/components/HeaderClient/index.tsx
@@ -17,7 +17,7 @@ export default function HeaderClient() {
     <header className="dsc-header-client">
       <nav className="dsc-container">
         <Link to="/catalog">
-          <h1>DSC</h1>
+          <h1>DSCommerce</h1>
         </Link>
         <div className="dsc-navbar-right">
           <div className="dsc-menu-items-container">
